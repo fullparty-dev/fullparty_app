ef37e19abfe53bc143e4918bad9932e7db0e33b8
Tasuku <fullparty.dev@gmail.com>
2025-07-18 19:26:04 +0900

    fix: 全体背景色をlayoutで統一して白帯を修正

--- src/app/layout.js
+++ src/app/layout.js
@@ -31,7 +31,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ja">
-      <body>{children}</body>
+      <body className="bg-primary text-white">{children}</body>
     </html>
   );
 }
